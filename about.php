af41d8113dc7c73a09910ba352d92dc21e4f2d25
KaplanDS3 <oriorigeva12@gmail.com>
2023-03-30 17:07:01 -0600

    1.5

--- about.php
+++ about.php
@@ -6,12 +6,12 @@ include 'header.php';
 ?>
 <h1><b>About Us</b></h1>
 
-<div class="flex-row" id="wrapper">
+<div class="about-row" id="wrapper">
     <div>
         <p class="about-paragraph">This website was created with the sole purpose of giving students the ability to view the school's athletic records, any time and any where!</p>
     </div>
     <div>
-        <img class="about-img" src="about_usimg.jpg" width="370 " height="300">
+        <img src="about_usimg.jpg" width="370 " height="300">
     </div>
 </div>
 
--- index.php
+++ index.php
@@ -8,13 +8,11 @@ include 'header.php';
 <div id="home">
 	<p>All the information you need to know about the athletic records set in sports events during the school's history in a simple and effective format!</p>
 </div>
-<div id="home">
 <div id="stage">
 	<img src="home_img1.jpg" width="460" height="300">
 	<img src="home_img2.jpg" width="460" height="300">
 	<img src="home_img3.jpg" width="460" height="300">
 	<img src="home_img4.jpg" width="460" height="300">
-</div>
 </div>
 
 <script>
