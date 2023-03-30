6197663db22e7761f60de9fd65afe88c873b073f
KaplanDS3 <oriorigeva12@gmail.com>
2023-03-30 12:15:30 -0600

    1.5

--- about.php
+++ about.php
@@ -4,7 +4,16 @@
 include 'connect.php';
 include 'header.php';
 ?>
+<h1><b>About Us</b></h1>
 
+<div class="flex" id="wrapper">
+    <div>
+        <p class="about-paragraph">This website was created with the sole purpose of giving students the ability to view the school's athletic records, any time and any where!</p>
+    </div>
+    <div>
+        <img class="about-img" src="about_usimg.jpg" width="370 " height="300">
+    </div>
+</div>
 
 
 <?php
