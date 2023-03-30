542052bd511e951ae3c7f3733295c1072533c889
KaplanDS3 <oriorigeva12@gmail.com>
2023-03-30 09:21:04 -0600

    1.4

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
-<html><title>School Athletic Records</title></html>
+<html><title>School Athletic Records</title>
 
 <?php
 //Implemintation of the header and connection to the database
 include 'connect.php';
 include 'header.php';
 ?>
-
-
+<div id="home">
+	<p>All the information needed to know about the athletic records set in sports events during the school's history in a simple and effective format!</p>
+</div>
 
 <?php
 include 'footer.php';
 ?>
+</html>
