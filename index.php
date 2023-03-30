97d2cddeb6022f2b4883982bf20d7dafa6b44121
KaplanDS3 <oriorigeva12@gmail.com>
2023-03-30 08:41:01 -0600

    1.3

--- index.php
+++ index.php
@@ -1,20 +1,11 @@
-<html>
-	<title>School Athletic Records</title>
-</html>
+<title>School Athletic Records</title>
 <?php
-//index.php
+//Implemintation of the header and connection to the database
 include 'connect.php';
 include 'header.php';
-
-echo '<tr>';
-	echo '<td class="leftpart">';
-		echo '<h3><a href="category.php?id=">Category name</a></h3> Category description goes here';
-	echo '</td>';
-	echo '<td class="rightpart">';				
-			echo '<a href="Home.php?id=">Topic subject</a> at 10-10';
-	echo '</td>';
-echo '</tr>';
 ?>
+
+
 
 <?php
 include 'footer.php';
