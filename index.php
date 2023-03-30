a06d79adc3b29d02b9d5b02a11390626073c4854
KaplanDS3 <oriorigeva12@gmail.com>
2023-03-30 10:35:25 -0600

    1.5

--- index.php
+++ index.php
@@ -8,6 +8,29 @@ include 'header.php';
 <div id="home">
 	<p>All the information needed to know about the athletic records set in sports events during the school's history in a simple and effective format!</p>
 </div>
+<div id="stage">
+<p>All the information needed to know about the athletic records set in sports events during the school's history in a simple and effective format!</p>
+	<img src="home_img1.jpg" width="460" height="300">
+	<img src="home_img2.jpg" width="460" height="300">
+	<img src="home_img3.jpg" width="460" height="300">
+	<img src="home_img4.jpg" width="460" height="300">
+</div>
+
+<script>
+ window.addEventListener("DOMContentLoaded", function(e) {
+
+// Original JavaScript code by Chirp Internet: chirpinternet.eu
+// Please acknowledge use of this code by including this header.
+
+var stage = document.getElementById("stage");
+var fadeComplete = function(e) { stage.appendChild(arr[0]); };
+var arr = stage.getElementsByTagName("a");
+for(var i=0; i < arr.length; i++) {
+  arr[i].addEventListener("animationend", fadeComplete, false);
+}
+
+}, false);
+</script>
 
 <?php
 include 'footer.php';
