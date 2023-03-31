4baaa8237c6796c3d290d2f5551386d639f66ccf
KaplanDS3 <oriorigeva12@gmail.com>
2023-03-31 09:07:02 -0600

    1.7

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 date_default_timezone_set('America/Mexico_City');
 //connect.php
 
-$conn = mysqli_connect("127.0.0.1","root","","account");
+$conn = mysqli_connect("127.0.0.1","root","","sports");
 
 if(mysqli_connect_error()){
     echo "Cannot Connect";
